2d37f0c0d598c0a11bcc51763ac6856870a71892
Aashish Panta <55815492+aashishpanta0@users.noreply.github.com>
2024-07-08 17:08:21 -0600

    check hooks permission for codespace push

--- .devcontainer/setup-hooks.sh
+++ .devcontainer/setup-hooks.sh
@@ -7,7 +7,7 @@ protected_branch='main'
 current_branch=$(git symbolic-ref --short HEAD)
 
 if [ "$current_branch" = "$protected_branch" ]; then
-  echo "You can't push to the $protected_branch branch from Codespaces."
+  echo "You can't push to the $protected_branch branch from Codespaces. Please contact the repo owner or Aashish Panta (aashishpanta0@gmail.com). Thank you!"
   exit 1
 fi
 EOF
